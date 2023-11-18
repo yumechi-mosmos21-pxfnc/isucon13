20b1fde979589c841143085842d4a0e5d71bf063
yumechi <yumechi0525@gmail.com>
2023-11-19 02:52:27 +0900

    :wrench: 作業用スクリプトを追加

--- analytics_tools/scp_local.sh
+++ analytics_tools/scp_local.sh
@@ -3,6 +3,6 @@
 hostuser="isucon"
 hostname="133.242.221.35"
 
-scp "$hostuser@$hostname":/home/isucon/copy_target_log/mysql-slow.log pt-query.log
-scp "$hostuser@$hostname":/home/isucon/copy_target_log/access_alp.log access_alp.log
-scp "$hostuser@$hostname":/home/isucon/copy_target_log/access_kataribe.log access_kataribe.log
+scp "$hostuser@$hostname":/home/isucon/copy_target_log/* .
+# ファイル名を補正
+cp mysql-slow.log pt-query.log
