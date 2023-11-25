69b2052c66a7df1def7ce350673a641729db3db9
mosmos21 <dev@mosmos21.net>
2023-11-25 12:28:42 +0900

    bindを忘れていました

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -551,12 +551,17 @@ async fn search_livestreams_handler(
         }
         sqlx::query_as(&query).fetch_all(&mut *tx).await?
     } else {
-        sqlx::query_as(vec![
+        let query_str = vec![
             "SELECT livestreams.* FROM livestreams",
             "INNER JOIN livestream_tags ON livestreams.id = livestream_tags.livestream_id",
             "INNER JOIN tags ON livestream_tags.tag_id = tags.id",
             "WHERE tags.name = ? ORDER BY livestreams.id DESC",
-        ].join(" ").as_str()).fetch_all(&mut *tx).await?
+        ].join(" ").as_str();
+        
+        sqlx::query_as(query_str)
+            .bind(key_tag_name)
+            .fetch_all(&mut *tx)
+            .await?
     };
 
     let livestreams = fill_livestreams_response(&mut tx, livestream_models).await?;
