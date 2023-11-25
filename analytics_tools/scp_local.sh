48acdd7d22396d2b9af897fbd6cd6430af11d75c
yumechi <yumechi0525@gmail.com>
2023-11-25 11:00:53 +0900

    解析結果

--- analytics_tools/scp_local.sh
+++ analytics_tools/scp_local.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 hostuser="isucon"
-hostname="133.242.221.35"
+hostname="35.79.124.212"
 
-scp "$hostuser@$hostname":/home/isucon/copy_target_log/* .
+scp "$hostuser@$hostname":/home/isucon/copy_target_log/access.log .
+scp "$hostuser@$hostname":/home/isucon/copy_target_log/access_alp.log .
+scp "$hostuser@$hostname":/home/isucon/copy_target_log/access_kataribe.log  .
+scp "$hostuser@$hostname":/home/isucon/copy_target_log/mysql-slow.log .
+scp "$hostuser@$hostname":/home/isucon/copy_target_log/mysql.log .
 # ファイル名を補正
 cp mysql-slow.log pt-query.log
